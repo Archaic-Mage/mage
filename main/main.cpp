8c46c73347c3c845d4cef70975d1b53dc0ffd108
Soham Tripathy <me.soham.tripathy@gmail.com>
2025-07-24 03:48:00 +0530

    MAGE-3 Add proto buf support

--- main/main.cpp
+++ main/main.cpp
@@ -2,6 +2,15 @@
 
 std::int32_t main() {
 
+    test::AddressBook myAddressBook;
+    auto* myPerson = myAddressBook.add_people();
+    myPerson->set_name("Soham");
+    myPerson->set_email("hi@mage.com");
+    auto* myPhone = myPerson->add_phones();
+    myPhone->set_number("957209570");
+    myPhone->set_type(test::Person_PhoneType_PHONE_TYPE_MOBILE);
+    Logger::testInfo(myAddressBook);
+    Logger::info(myPerson->email());
     Logger::info("Hello World!");
     return 0;
 }
--- main/main.hpp
+++ main/main.hpp
@@ -3,10 +3,17 @@
 #include <string>
 #include <iostream>
 
+#include "protocols/test-proto/test.pb.h"
+
 struct Logger 
 {
     inline static void info(std::string aMessage)
     {
         std::cout << "[INFO] " << aMessage << std::endl;
+    }
+
+    inline static void testInfo(test::AddressBook const& aAddressBook)
+    {
+        Logger::info(aAddressBook.SerializeAsString());
     }
 };
