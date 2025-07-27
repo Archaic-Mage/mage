67a92a50dcb33f0403229aa1e6035de64a24cebd
Soham Tripathy <me.soham.tripathy@gmail.com>
2025-07-28 00:49:55 +0530

    MAGE-17 Remove old logging method
    Change-Id: I6f95791d9fbbac4b5f3d90c44f5cff8040969d91

--- main/main.hpp
+++ main/main.hpp
@@ -7,16 +7,4 @@
 
 namespace mage
 {
-struct Logger
-{
-    inline static void info(std::string aMessage)
-    {
-        std::cout << "[INFO] " << aMessage << std::endl;
-    }
-
-    inline static void testInfo(test::AddressBook const& aAddressBook)
-    {
-        Logger::info(aAddressBook.SerializeAsString());
-    }
-};
 } // namespace mage
