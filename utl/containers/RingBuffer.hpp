e7b65e151cec8bd80035dca58eab34ffc63d7c76
Soham Tripathy <me.soham.tripathy@gmail.com>
2025-07-30 19:08:26 +0530

    MAGE-#8 Implement RingBuffer
    This commit would implement a generic ring buffer implementation that
    supports two modes:
     - Single Producer, Single Consumer
     - Single Producer, Multiple Consumers
    
    The implementation also includes:
     - RingBuffer: the core data structure
      - Shared Circular Memory
      - File-Based Writing
     - Producer: interface or class for writing to the ring buffer
     - Consumer: interface or class for reading from the ring buffer
    
    Change-Id: Id7a5724dcea652f902dfd1b0ca1824bee6c2dac7

---
+++ utl/containers/RingBuffer.hpp
@@ -0,0 +1,176 @@
+#pragma once
+
+#include <array>
+#include <atomic>
+#include <cstdint>
+#include <cstring>
+#include <fstream>
+#include <memory>
+#include <span>
+
+namespace mage::utl
+{
+
+class StorageInterface
+{
+public:
+    virtual ~StorageInterface() = default;
+    virtual std::size_t getStorageCapacity() = 0;
+    virtual void write(std::size_t anIndex, std::span<const std::byte> aData) = 0;
+    virtual void read(std::size_t anIndex, std::span<std::byte> aOutBuffer) = 0;
+};
+
+template <std::size_t SizeV>
+class SharedMemoryBuffer final : public StorageInterface
+{
+public:
+    SharedMemoryBuffer() = default;
+
+    std::size_t getStorageCapacity() override
+    {
+        return SizeV;
+    }
+
+    void write(const std::size_t anIndex, const std::span<const std::byte> aData) override
+    {
+        if (anIndex + aData.size() > SizeV)
+        {
+            throw std::out_of_range("SharedMemoryBuffer::write");
+        }
+        std::memcpy(theBuffer.data() + anIndex, aData.data(), aData.size());
+    }
+
+    void read(const std::size_t anIndex, std::span<std::byte> aOutBuffer) override
+    {
+        if (anIndex + aOutBuffer.size() > SizeV)
+        {
+            throw std::out_of_range("SharedMemoryBuffer::read");
+        }
+        std::memcpy(aOutBuffer.data(), theBuffer.data() + anIndex, aOutBuffer.size());
+    }
+
+private:
+    std::array<std::byte, SizeV> theBuffer;
+};
+
+template <std::size_t SizeV>
+class FileMemoryBuffer final : public StorageInterface
+{
+public:
+    explicit FileMemoryBuffer(const std::string& aFileName) :
+            theFile(aFileName, std::ios::in | std::ios::out | std::ios::binary | std::ios::trunc)
+    {
+        if (!theFile)
+        {
+            throw std::runtime_error("Failed to open file");
+        }
+        // Allocate file size
+        theFile.seekp(SizeV);
+        constexpr char myZero = 0;
+        theFile.write(&myZero, 1);
+        theFile.flush();
+    }
+
+    std::size_t getStorageCapacity() override
+    {
+        return SizeV;
+    }
+
+    void write(const std::size_t anIndex, const std::span<const std::byte> aData) override
+    {
+        if (anIndex + aData.size() > SizeV)
+        {
+            throw std::out_of_range("FileMemoryBuffer::write");
+        }
+        theFile.seekp(anIndex);
+        theFile.write(reinterpret_cast<const char*>(aData.data()), aData.size());
+        theFile.flush();
+    }
+
+    void read(const std::size_t anIndex, std::span<std::byte> aOutBuffer) override
+    {
+        if (anIndex + aOutBuffer.size() > SizeV)
+        {
+            throw std::out_of_range("FileMemoryBuffer::read");
+        }
+        theFile.seekg(anIndex);
+        theFile.read(reinterpret_cast<char*>(aOutBuffer.data()), aOutBuffer.size());
+    }
+
+private:
+    std::fstream theFile;
+};
+
+template <typename DataTypeT>
+class RingBuffer
+{
+public:
+    explicit RingBuffer(std::unique_ptr<StorageInterface> anStorage) :
+            theCapacity(anStorage->getStorageCapacity() / sizeof(DataTypeT)),
+            theBufferStorage(std::move(anStorage))
+    {
+    }
+
+    class Producer
+    {
+    public:
+        explicit Producer(RingBuffer& aBuffer) : theBuffer(aBuffer) {}
+
+        bool write(const std::span<const DataTypeT> aData)
+        {
+            return theBuffer.pushImpl(aData);
+        }
+
+    private:
+        RingBuffer& theBuffer;
+    };
+
+    class Consumer
+    {
+    public:
+        explicit Consumer(RingBuffer& aBuffer) : theBuffer(aBuffer) {}
+
+        bool read(std::span<DataTypeT> aOutputData)
+        {
+            return theBuffer.pullImpl(aOutputData);
+        }
+
+    private:
+        RingBuffer& theBuffer;
+    };
+
+private:
+    bool pushImpl(const std::span<const DataTypeT> aWriteableData)
+    {
+        const auto myCurrPushIndex = thePushIndex.load(std::memory_order_relaxed);
+        const auto myWriteSize = aWriteableData.size();
+        const auto myNextPushIndex = (myCurrPushIndex + myWriteSize) % theCapacity;
+        if (myNextPushIndex == thePullIndex.load(std::memory_order_acquire))
+        {
+            return false;
+        }
+        theBufferStorage->write(myCurrPushIndex * sizeof(DataTypeT), std::as_bytes(aWriteableData));
+        thePushIndex.store(myNextPushIndex, std::memory_order_release);
+        return true;
+    }
+
+    bool pullImpl(std::span<DataTypeT> aOutputData)
+    {
+        const auto myCurrPullIndex = thePullIndex.load(std::memory_order_relaxed);
+        const auto myReadSize = aOutputData.size();
+        const auto myNextPullIndex = (myCurrPullIndex + myReadSize) % theCapacity;
+        if (myNextPullIndex > thePushIndex.load(std::memory_order_acquire))
+        {
+            return false;
+        }
+        theBufferStorage->read(myCurrPullIndex * sizeof(DataTypeT), std::as_writable_bytes(aOutputData));
+        thePullIndex.store(myNextPullIndex, std::memory_order_release);
+        return true;
+    }
+
+    std::size_t theCapacity;
+    std::unique_ptr<StorageInterface> theBufferStorage;
+    std::atomic<std::int32_t> thePushIndex; // points to index to push new data
+    std::atomic<std::int32_t> thePullIndex; // points to index to pull data
+};
+} // namespace mage::utl
---
+++ utl/containers/test/RingBufferTest.cpp
@@ -0,0 +1,149 @@
+#include "utl/containers/RingBuffer.hpp"
+
+#include <thread>
+
+#include <gtest/gtest.h>
+
+namespace mage::utl::test
+{
+class RingBufferTest : public ::testing::Test
+{
+public:
+    struct Data
+    {
+        std::int32_t theIndex;
+        std::int32_t theValue;
+    };
+
+    void SetUp() override
+    {
+        theSharedMemoryBuffer = std::make_unique<SharedMemoryBuffer<MaxSize>>();
+        theFileMemoryBuffer = std::make_unique<FileMemoryBuffer<MaxSize>>("/tmp/test.buf");
+        theRingBuffer = std::make_unique<RingBuffer<Data>>(std::make_unique<SharedMemoryBuffer<MaxSize>>());
+    }
+
+    static Data createData()
+    {
+        return Data{.theIndex = DefaultIndex, .theValue = DefaultValue};
+    }
+
+protected:
+    static constexpr std::int64_t MaxSize = 8 * 1024 * 1024;
+    static constexpr std::int32_t DefaultIndex = 12;
+    static constexpr std::int32_t DefaultValue = 10;
+    std::unique_ptr<SharedMemoryBuffer<MaxSize>> theSharedMemoryBuffer;
+    std::unique_ptr<FileMemoryBuffer<MaxSize>> theFileMemoryBuffer;
+    std::unique_ptr<RingBuffer<Data>> theRingBuffer;
+};
+
+TEST_F(RingBufferTest, WriteAndReadDataFromSharedBuffer)
+{
+    std::array<std::int64_t, 5> myData;
+    for (std::size_t myI = 0; myI < 5; ++myI)
+    {
+        myData[myI] = 100 + myI;
+    }
+    theSharedMemoryBuffer->write(0, std::as_bytes(std::span(myData)));
+    std::int64_t myReadNum = 0;
+    for (std::size_t myI = 0; myI < 5; ++myI)
+    {
+        constexpr auto mySizeOfInteger = sizeof(std::int64_t);
+        theSharedMemoryBuffer->read(
+            myI * mySizeOfInteger,
+            std::span(reinterpret_cast<std::byte*>(&myReadNum), mySizeOfInteger));
+        EXPECT_EQ(myReadNum, myData[myI]);
+    }
+}
+
+TEST_F(RingBufferTest, WriteAndReadDataFromFileBuffer)
+{
+    std::array<std::int64_t, 5> myData;
+    for (std::size_t myI = 0; myI < 5; ++myI)
+    {
+        myData[myI] = 100 + myI;
+    }
+    theFileMemoryBuffer->write(0, std::as_bytes(std::span(myData)));
+    std::int64_t myReadNum = 0;
+    for (std::size_t myI = 0; myI < 5; ++myI)
+    {
+        constexpr auto mySizeOfInteger = sizeof(std::int64_t);
+        theFileMemoryBuffer->read(
+            myI * mySizeOfInteger,
+            std::span(reinterpret_cast<std::byte*>(&myReadNum), mySizeOfInteger));
+        EXPECT_EQ(myReadNum, myData[myI]);
+    }
+}
+
+TEST_F(RingBufferTest, SingleThreadedReadAndWrite)
+{
+    std::array<Data, 5> myData;
+    for (std::size_t myI = 0; myI < 5; ++myI)
+    {
+        myData.at(myI) = createData();
+    }
+
+    auto myProducer = RingBuffer<Data>::Producer(*theRingBuffer);
+    auto myConsumer = RingBuffer<Data>::Consumer(*theRingBuffer);
+
+    EXPECT_TRUE(myProducer.write(std::span(myData)));
+
+    std::array<Data, 5> myReadBuffer;
+    EXPECT_TRUE(myConsumer.read(std::span(myReadBuffer)));
+    for (std::size_t myI = 0; myI < 5; ++myI)
+    {
+        EXPECT_EQ(myReadBuffer.at(myI).theIndex, myData.at(myI).theIndex);
+        EXPECT_EQ(myReadBuffer.at(myI).theValue, myData.at(myI).theValue);
+    }
+
+    EXPECT_FALSE(myConsumer.read(std::span(myReadBuffer)));
+}
+
+TEST_F(RingBufferTest, MultiThreadedReadAndWrite)
+{
+    std::array<Data, 1024> myData;
+    std::ranges::fill_n(myData.begin(), myData.size(), createData());
+
+    std::thread myProducerThread(
+        [this, &myData]()
+        {
+            auto myProducer = RingBuffer<Data>::Producer(*theRingBuffer);
+            while (myProducer.write(std::span(myData.begin(), 8)))
+            {
+                std::this_thread::yield();
+            }
+        });
+
+    std::vector<Data> myReadData;
+
+    std::thread myConsumerThread(
+        [this, &myData, &myReadData]()
+        {
+            auto myConsumer = RingBuffer<Data>::Consumer(*theRingBuffer);
+            std::array<Data, 4> myReadBuffer;
+            while (myReadData.size() < myData.size())
+            {
+                if (myConsumer.read(std::span(myReadBuffer)))
+                {
+                    for (auto& myRead : myReadBuffer)
+                    {
+                        myReadData.push_back(myRead);
+                    }
+                }
+                else
+                {
+                    std::this_thread::yield();
+                }
+            }
+        });
+
+    myProducerThread.join();
+    myConsumerThread.join();
+
+    for (std::size_t myI = 0; myI < myData.size(); ++myI)
+    {
+        EXPECT_EQ(myReadData.at(myI).theIndex, myData.at(myI).theIndex);
+        EXPECT_EQ(myReadData.at(myI).theValue, myData.at(myI).theValue);
+    }
+}
+
+} // namespace mage::utl::test
